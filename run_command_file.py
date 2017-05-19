6167750b0afc397ec117b6337585923ed9387b3e
moulaali <hi.moulaali@gmail.com>
2017-05-19 16:29:25 -0700

    Create run_command_file.py

---
+++ run_command_file.py
@@ -0,0 +1,20 @@
+'''
+A sample program to execute commands for specific files in a directory.
+The directory structure is walked in recursive fashion
+'''
+
+import os
+import sys
+import fnmatch
+from subprocess import call
+
+cwd = os.getcwd().split(os.sep)[-1]
+if (cwd != 'foo'):
+    print "Error: Run this script from foo directory"
+    exit()
+
+blah_dir = os.getcwd() + os.sep + "bar/blah"
+for dir_path, dirnames, files in os.walk(borg_dir):
+    for file in  fnmatch.filter(files, 'foo.txt'):
+        full_file_path = os.path.join(dir_path, file)
+        call(["ls", full_file_path)
